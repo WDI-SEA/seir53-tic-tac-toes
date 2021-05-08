e2110a0d9050200bd6e649bfa88c47eed4a4892f
Consecratio <jcasique@gmail.com>
2021-05-08 10:31:31 -0700

    tic tac toe game - start

---
+++ scripts/app.js
@@ -0,0 +1,128 @@
+window.addEventListener("DOMContentLoaded", () => {
+    // setup variables that will be needed globally
+    let gameTiles = document.querySelectorAll(".gameTile")
+    let nextMove = "X"
+    let currentPlayer = document.querySelector("#nextPlayer")
+    let roundSpan = document.querySelector("#rounds")
+    let winningPlayer
+    let roundNum = 1
+    let boardArr = [["", "", ""], ["", "", ""], ["", "", ""]]
+    
+    // loop through the tiles and add event listeners
+    for (let i = 0; i < gameTiles.length; i++) {
+        gameTiles[i].addEventListener("click", (event) => {
+            // track and display rounds
+            roundNum++
+            roundSpan.innerText = roundNum
+            // see what current letter is of game tile and toggle between whichever letter is current vs x or o
+            // dont allow players to change an already clicked button
+            if (event.target.className != "gameTile clicked") {
+                // set X or O on clicked div
+                event.target.innerText = nextMove
+                // check if player who just went won
+                winningPlayer = gameState(nextMove)
+                if (winningPlayer) {
+                    // found winner
+                    gameEnd(`PLAYER ${winningPlayer} WINS!`)
+                    return
+                } else if (roundNum === 10){
+                    // no winner
+                    gameEnd("DRAW")
+                    return
+                }
+
+                // change nextMove to X or O depending on what was just set
+                nextMove = nextMove === "X" ? "O" : "X" 
+                // update the span that states who's turn it is
+                currentPlayer.innerText = `It's ${nextMove}'s turn`
+            }
+            // add class to cell that was clicked
+            event.target.className = "gameTile clicked"
+        })
+    }
+
+    function gameState(lastPlay) {
+        // setup 2D array
+        let winCheck = []
+        let tilesIter = 0
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                boardArr[i][j] = gameTiles[tilesIter].innerText
+                tilesIter += 1
+            }
+        }
+        // check rows
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                winCheck.push(boardArr[i][j])
+                if (j === 2) {
+                    if (winCheck.every( x => x === lastPlay )) {
+                        console.log(`${lastPlay} wins in a row`)
+                        return lastPlay
+                    }
+                    winCheck = []
+                }
+            }
+        }
+        // check cols
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                winCheck.push(boardArr[j][i])
+                if (j === 2) {
+                    if (winCheck.every( x => x === lastPlay )) {
+                        console.log(`${lastPlay} wins in a col`)
+                        return lastPlay
+                    }
+                    winCheck = []
+                }
+            }
+        }
+        // check diag top left to bottom right
+        for (let i = 0; i < 3; i++) {
+            winCheck.push(boardArr[i][i])
+            if (i === 2) {
+                if (winCheck.every( x => x === lastPlay )) {
+                    console.log(`${lastPlay} wins in a dia`)
+                    return lastPlay
+                }
+                winCheck = []
+            }
+        }
+        // check diag top right to bottom left
+        for (let i = 0, j = 2; i < 3; i++, j--) {
+            winCheck.push(boardArr[i][j])
+            if (i === 2) {
+                if (winCheck.every( x => x === lastPlay )) {
+                    console.log(`${lastPlay} wins in a dia`)
+                    return lastPlay
+                }
+                winCheck = []
+            }
+        }
+    }
+
+    function gameEnd(endMessage) {
+        for (let i = 0; i < gameTiles.length; i++) {
+            gameTiles[i].className = "gameTile clicked"
+        }
+        currentPlayer.innerText = endMessage
+    }
+
+    function gameReset() {
+        // TODO: Reset the game without refreshing the page
+    }
+    
+    // BONUS
+    // implement reset button without refreshing a page
+
+    // track player's wins over time
+
+    // add simple AI to have CPU vs Player
+    
+    // add time delay for AI moves to make it more "human"
+
+    // SUPER BONUS
+    // add AI that can win
+
+    
+})
