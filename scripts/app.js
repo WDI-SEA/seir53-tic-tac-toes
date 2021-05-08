7c3b8469d68fbf9feb95e1367c8a4228bce0fdcd
BeckyBoo88 <rbooco07@gmail.com>
2021-05-08 14:01:06 -0600

    SecSavTTT

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,45 @@
 window.addEventListener('DOMContentLoaded', () => {
-   // On click toggle between Xs and Os on the gameboard
+   
+ // ARRAY FOR SQUARES
+    let s1 = document.getElementById('one')
+    let s2= document.getElementById('two')
+    let s3= document.getElementById('three')
+    let s4= document.getElementById('four')    
+    let s5= document.getElementById('five')
+    let s6= document.getElementById('six')
+    let s7= document.getElementById('seven')
+    let s8= document.getElementById('eight')
+    let s9= document.getElementById('nine')
+
+    const gameSq= [s1,s2,s3,s4,s5,s6,s7,s8]
+
+// WIN.LOSE.TIE CONDITIONAL
+
+    const xWins= {
+        scenario1 : (s1,s2,s3),
+        scenario2 : (s4,s5,s6),
+        scenario3 : (s7,s8,s9),
+        scenario4 : (s1,s4,s7),
+        scenario5 : (s2,s5,s3),
+        scenario6 : (s3,s6,s9),
+        scenario7 : (s1,s5,s9),
+        scenario8 : (s3,s5,s7)
+    }
+
+    if xWins === true {
+        console.log("X wins! O is oooo so sad");
+    } else if (oWins === true) {
+        console.log("O wins! X is xxxceptionally distraught");
+    } else {
+        console.log("Tie! Everyone's a winner!! kinda..")
+    }
+   
+   
+   
+   
+   
+   
+    // On click toggle between Xs and Os on the gameboard
    let gameTiles = document.querySelectorAll('.gameTile')
 
    // Click event for tiles. Every tile is X unless already clicked, then 0
@@ -9,9 +49,7 @@ window.addEventListener('DOMContentLoaded', () => {
            let tile = gameTiles[i].innerText
           
            // toggle between whichever letter is current vs x or 0
-           let newPlayStatus = tile === "X" ? "0" : "X"
-           
-           
+           let newPlayStatus = tile === "X" ? "O" : "X"
            
            // set innerText of div to be new toggled letter
            gameTiles[i].innerText = newPlayStatus
@@ -19,15 +57,16 @@ window.addEventListener('DOMContentLoaded', () => {
    }
    
 
-// Player switch between X and O
-// function switchPlayers(){
-//     if (currentPlayer === "X") {
-//         currentPlayer = 'O';
-//     } else {
-//         currentPlayer = 'X';
-//     }
-// }
-// Need click event to call function
+// RESET BUTTON
+    let gameTileList= document.getElementsByClassName('gameTile')
+    let clear= document.getElementById('Reset')
+
+    clear.addEventListener('click', () => {
+        for (let v=0; v < gameTileList.length; v++) {
+            let tile =gameTileList[v].innerText
+            gameTileList[v].innerText = " "
+         }
+    })
 
 
 
