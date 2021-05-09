fb6a5fa2524c404d9b9f6f8875cdaf66287558ee
Consecratio <jcasique@gmail.com>
2021-05-09 11:57:37 -0700

    setup simple ai and working on minimax algo

--- scripts/app.js
+++ scripts/app.js
@@ -7,7 +7,7 @@ window.addEventListener("DOMContentLoaded", () => {
     let nextMove = "X"
     let currentPlayer = document.querySelector("#nextPlayer")
     let roundSpan = document.querySelector("#rounds")
-    let winningPlayer
+    let winningPlayer = null
     let roundNum = 1
     let boardArr = [["", "", ""], ["", "", ""], ["", "", ""]]
 
@@ -17,11 +17,11 @@ window.addEventListener("DOMContentLoaded", () => {
     let pOScore = 0
     
     gameStart()
-    // loop through the tiles and add event listeners
+
     function gameStart() {
+        // loop through the tiles and add event listeners
         for (let i = 0; i < gameTiles.length; i++) {
             gameTiles[i].addEventListener("click", (event) => {
-                // see what current letter is of game tile and toggle between whichever letter is current vs x or o
                 // dont allow players to change an already clicked button
                 if (event.target.className != "gameTile clicked") {
                     // track and display rounds
@@ -50,6 +50,9 @@ window.addEventListener("DOMContentLoaded", () => {
                     nextMove = nextMove === "X" ? "O" : "X" 
                     // update the span that states who's turn it is
                     currentPlayer.innerText = `It's ${nextMove}'s turn`
+
+                    //TEST - if O's turn, have CPU play
+                    nextMove === "O" ? cpuPlayer() : console.log("Not CPU's Turn")
                 }
                 // add class to cell that was clicked
                 event.target.className = "gameTile clicked"
@@ -148,4 +151,88 @@ window.addEventListener("DOMContentLoaded", () => {
         // restart game
         gameStart()
     } 
+
+    function cpuPlayer() {
+        // TODO: Add AI logic for making moves
+
+        /*
+        // START OF BASIC AI
+        // basic AI that picks a random spot on the board
+        let randNum = Math.floor(Math.random() * 9)
+        while (gameTiles[randNum].innerText != "") {
+            randNum = Math.floor(Math.random() * 9)
+        }
+        // simulate click on random tile that is open
+        gameTiles[randNum].click()
+        // END OF BASIC AI
+        */
+        
+        // TEST - try following along with setting up AI
+        let bestScore = -Infinity;
+        let move;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+            // Is the spot available?
+            if (boardArr[i][j] == "") {
+                boardArr[i][j] = "O";
+                let score = minimax(boardArr, 0, false);
+                boardArr[i][j] = "";
+                // console.log(score)
+                // console.log(bestScore)
+                if (score > bestScore) {
+                bestScore = score;
+                move = { i, j };
+                }
+            }
+            }
+        }
+        // this is where the AI makes it's move
+        // boardArr[move.i][move.j] = "O";
+        gameTiles[ ((move.i * 3) + move.j) ].click()
+    }
+
+    let scores = {
+        X: -1,
+        O: 1,
+        tie: 0
+    }
+
+    function minimax(board, depth, isMaximizing) {
+        let result = gameState(nextMove);
+        if (result !== null) {
+            console.log(result) // WHAT ARE RESULTS?
+            return scores[result];
+        }
+      
+        if (isMaximizing) {
+          let bestScore = -Infinity;
+          for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+              // Is the spot available?
+              if (board[i][j] == '') {
+                board[i][j] = "O";
+                let score = minimax(board, depth + 1, false);
+                board[i][j] = '';
+                bestScore = max(score, bestScore);
+              }
+            }
+          }
+          return bestScore;
+        } else {
+          let bestScore = Infinity;
+          for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+              // Is the spot available?
+              if (board[i][j] == '') {
+                board[i][j] = "X";
+                let score = minimax(board, depth + 1, true);
+                board[i][j] = '';
+                bestScore = min(score, bestScore);
+              }
+            }
+          }
+          return bestScore;
+        }
+      }
+
 })
