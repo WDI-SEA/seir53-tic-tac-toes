ef3ac701e641e75e99444897d49d012f83be0625
Consecratio <jcasique@gmail.com>
2021-05-08 21:51:25 -0700

    added scores, rounds, and reset button functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
+    // TESTING
+    document.getElementById("reset").addEventListener("click", gameReset)
+    // TESTING
+
     // setup variables that will be needed globally
     let gameTiles = document.querySelectorAll(".gameTile")
     let nextMove = "X"
@@ -7,44 +11,57 @@ window.addEventListener("DOMContentLoaded", () => {
     let winningPlayer
     let roundNum = 1
     let boardArr = [["", "", ""], ["", "", ""], ["", "", ""]]
-    
-    // loop through the tiles and add event listeners
-    for (let i = 0; i < gameTiles.length; i++) {
-        gameTiles[i].addEventListener("click", (event) => {
-            // track and display rounds
-            roundNum++
-            roundSpan.innerText = roundNum
-            // see what current letter is of game tile and toggle between whichever letter is current vs x or o
-            // dont allow players to change an already clicked button
-            if (event.target.className != "gameTile clicked") {
-                // set X or O on clicked div
-                event.target.innerText = nextMove
-                // check if player who just went won
-                winningPlayer = gameState(nextMove)
-                if (winningPlayer) {
-                    // found winner
-                    gameEnd(`PLAYER ${winningPlayer} WINS!`)
-                    return
-                } else if (roundNum === 10){
-                    // no winner
-                    gameEnd("DRAW")
-                    return
-                }
 
-                // change nextMove to X or O depending on what was just set
-                nextMove = nextMove === "X" ? "O" : "X" 
-                // update the span that states who's turn it is
-                currentPlayer.innerText = `It's ${nextMove}'s turn`
-            }
-            // add class to cell that was clicked
-            event.target.className = "gameTile clicked"
-        })
+    // variables to track X and O's scores but not reset with reset button
+    let pXScore = 0
+    let pOScore = 0
+    
+    gameStart()
+    // loop through the tiles and add event listeners
+    function gameStart() {
+        for (let i = 0; i < gameTiles.length; i++) {
+            gameTiles[i].addEventListener("click", (event) => {
+                // see what current letter is of game tile and toggle between whichever letter is current vs x or o
+                // dont allow players to change an already clicked button
+                if (event.target.className != "gameTile clicked") {
+                    // track and display rounds
+                    roundNum++
+                    roundSpan.innerText = roundNum
+                    // set X or O on clicked div
+                    event.target.innerText = nextMove
+                    // check if player who just went won
+                    winningPlayer = gameState(nextMove)
+                    if (winningPlayer) {
+                        // found winner
+                        gameEnd(`PLAYER ${winningPlayer} WINS!`)
+                        // update player score
+                        winningPlayer === "X" ? pXScore++ : pOScore++
+                        document.getElementById(`playerXScore`).innerText = pXScore
+                        document.getElementById(`playerOScore`).innerText = pOScore
+                        return
+                    } else if (roundNum === 10){
+                        // no winner
+                        gameEnd("DRAW")
+                        document.getElementById(`playerXScore`).innerText = pXScore
+                        document.getElementById(`playerOScore`).innerText = pOScore
+                        return
+                    }
+                    // change nextMove to X or O depending on what was just set
+                    nextMove = nextMove === "X" ? "O" : "X" 
+                    // update the span that states who's turn it is
+                    currentPlayer.innerText = `It's ${nextMove}'s turn`
+                }
+                // add class to cell that was clicked
+                event.target.className = "gameTile clicked"
+            })
+        }
     }
 
     function gameState(lastPlay) {
         // setup 2D array
         let winCheck = []
         let tilesIter = 0
+        // setup a 2d array of the board
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 boardArr[i][j] = gameTiles[tilesIter].innerText
@@ -110,10 +127,30 @@ window.addEventListener("DOMContentLoaded", () => {
 
     function gameReset() {
         // TODO: Reset the game without refreshing the page
+        // reset starting variables
+        gameTiles = document.querySelectorAll(".gameTile")
+        nextMove = "X"
+        currentPlayer = document.querySelector("#nextPlayer")
+        roundSpan = document.querySelector("#rounds")
+        winningPlayer
+        roundNum = 1
+        boardArr = [["", "", ""], ["", "", ""], ["", "", ""]]
+        
+        // blank out played squares and reset div classes
+        for (let i = 0; i < gameTiles.length; i++) {
+            gameTiles[i].removeEventListener
+            gameTiles[i].className = "gameTile"
+            gameTiles[i].innerText = ""
+        }
+
+        currentPlayer.innerText = "It's X's turn."
+        roundSpan.innerText = "1"
+
+        // restart game
+        gameStart()
     }
     
     // BONUS
-    // implement reset button without refreshing a page
 
     // track player's wins over time
 
