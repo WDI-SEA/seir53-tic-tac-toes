6cd430a0609ec820718ab9587e629e158f51c3fb
erikwinkel <erikpwinkel@gmail.com>
2021-05-10 15:31:13 -0700

    removed debugging console.logs & changed names to be clearer

--- scripts/app.js
+++ scripts/app.js
@@ -24,7 +24,7 @@ const winStates = [[1,1,1,//1
                     1,0,0]]
 
 let gameState = [0,0,0,0,0,0,0,0,0]
-let playerTurn = true
+let playerOneTurn = true
 let gameOver = false
 let winner = ""
 let possWins = [[]]
@@ -34,13 +34,12 @@ window.addEventListener("DOMContentLoaded", () => {
     let gameTiles = document.querySelectorAll(".game-tile")
     possWins = winStates.slice()
 
-
     for(let i = 0; i < gameTiles.length; i++) {
         gameTiles[i].addEventListener("click", () => {
             if(!gameOver){ //if game is over, do nothing
                 if(!gameState[i]) { //if tile has been played, do nothing
                     //add x or o depending on whose turn it is
-                    if(playerTurn){
+                    if(playerOneTurn){
                         gameState[i] = "x"
                         gameTiles[i].classList.add("player-one")
                     } else {
@@ -48,19 +47,19 @@ window.addEventListener("DOMContentLoaded", () => {
                         gameTiles[i].classList.add("player-two")
                     }
                     //check if someone won
-                    uglyBruteForceChecker(i)
+                    checkGameState(i)
                     //change turn
                     if(gameOver){
                         if(!possWins.length) {
                             winner = "Draw!"
                         }else {
-                            winner = playerTurn ? "Player 1 wins!" : "Player 2 wins!"
+                            winner = playerOneTurn ? "Player 1 wins!" : "Player 2 wins!"
                         }
                         document.querySelector("#user-message").innerText = winner
                     } else {
-                        playerTurn = !playerTurn
+                        playerOneTurn = !playerOneTurn
                         //change turn display
-                        if(playerTurn){
+                        if(playerOneTurn){
                             document.querySelector("#user-message").innerText = "Player 1's Turn"
                         } else {
                             document.querySelector("#user-message").innerText = "Player 2's Turn"
@@ -70,11 +69,6 @@ window.addEventListener("DOMContentLoaded", () => {
                 }
 
             }
-                
-
-
-
-
         })
     }
 
@@ -84,43 +78,32 @@ window.addEventListener("DOMContentLoaded", () => {
             gameTiles[i].classList.remove("player-one")
             gameTiles[i].classList.remove("player-two")
         }
-        console.log("Resetting")
         gameOver = false
-        playerTurn = true
+        playerOneTurn = true
         winner = ""
         gameState = [0,0,0,0,0,0,0,0,0]
         possWins = winStates.slice()
         document.querySelector("#user-message").innerText = "Player 1's Turn"
-    })
-
-
-    
+    })    
 })
 
 //I know there's a better way don't do this
-function uglyBruteForceChecker(move) {
+function checkGameState(move) {
     //check each remaining win condition
-    console.log("START NEW CHECK HERE")
     let i = 0
     while(i < possWins.length) {
-        console.log("Possible Wins Remaining: " + possWins.length)
-        console.log("--Checking array: " + i)
         //check only check wins containing most recent move
         if(possWins[i][move]) {
-            console.log("--Possible win found...")
             let j = 0
             let counter = 0
             while(j < 9) {
                 if(possWins[i][j] && gameState[j]){
-                    console.log("----checking tile in play...")
                     if(gameState[j] !== gameState[move]) {
                         possWins.splice(i,1)
-                        console.log("------Removed a possible win at: " + i)
                         i--
                         break
                     } else {
                         counter++
-                        console.log("------count " + counter)
                     }
                 }
                 j++
