74e76ae6681687f8f0f4b0c4abe153190b3af2a6
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-05-09 23:47:58 -0400

    winner array, clear button, winning combo?

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ window.addEventListener("DOMContentLoaded", () => {
     let totalMoves = 0;
     let gameTiles = document.querySelectorAll(".gameTile")
 
- for(let i = 0; i < gameTiles.length; i++) {
+    for(let i = 0; i < gameTiles.length; i++) {
         gameTiles[i].addEventListener("click", (event) => {
 //         //see what current letter is
         //totalMoves++;
@@ -37,68 +37,62 @@ window.addEventListener("DOMContentLoaded", () => {
                 userMessage.innerText = "Player 2 Turn"
             }
         }
-        })}
+    })
+    }
+    const playerArray = [       
+        ["one", "two", "three"],
+        ["four", "five", "six"],
+        ["seven", "eight", "nine"],
+        ["one", "four", "seven"],
+        ["two", "five", "eight"],
+        ["three", "six", "nine"],
+        ["three", "five", "seven"],
+        ["one", "five", "nine"]
+    ]
+ 
+    console.log(playerArray.includes(["one", "two", "three"]));
+    //let winnerBanner = document.getElementById("winnerCircle");
+   // let checkIfPlayerWins = (playerArray, target) => target.every(t ); =>  
 
+   //     function checkIfPlayerWins (playerArray) {
+//         let (i = 0; i < playerArray.length; i++) { 
+//     if (playerXMoves === playerArray[i] || playerOMoves === playerArray[i]) {
+//         console.log("this player won!!");
+//     }    let winnerBanner = document.getElementById("winnerCircle");
+// }}
+//}
 // //  }    
 //gameTiles.forEach(() => {
 //addEventListener("click", (event) => {
 //gameTiles.innerText[i]
-//UPDATE:
-// toggle every other click to be X or O
-//})
-// once innerHTML is set disable further click events from happening on tile
-//// if clicked:
-// add a class that includes relevant styling for player X vs player O
 // let currentStatus = event.target.innerText;
 // let newPlayStatus = currentStatus === "X" ? "0" : "X"
 // event.target.innerHTML = newPlayStatus
 // CHECK FOR WIN
-    //one && three && five // write out all possible combinations for a win
-    // write out all possible combinations for a tie 
+
+// write out all possible combinations for a tie
+//detect Draw("Tied game""No winner") conditions 
     // capture gameboard elements
     // see if current gameboard element methods match either win or tie conditions
     // if true: return relevant message to message div
-    // disable clicks on gameboard
-    // re-set game board
+
     // else if: allow for play
-// const winner = [
-//     ["one", "two", "three"],
-//     ["four", "five", "six"],
-//     ["seven", "eight", "nine"],
-//     ["one", "four", "seven"],
-//     ["two", "five", "eight"],
-//     ["three", "six", "nine"],
-//     [],
-//     []
-// ]
+
          // array of arrays and comapre compare player array vs win array
         //create and event listener for click" on div .append
-        
-        //append should not respond to second click on div
-
-        //once player "wins", remaining empty cells should not accept new value
-        //detect Draw("Tied game""No winner") conditions
+    
         //detect winner - Stops game
         //detect winner - declares winner if one player gets 3 divs in a row
-        
-        //display message that indicates whos turn it is "X turn""O turn"
-        //let playerTurnDiv = document.querySelector("#userMessage")
-        //let player
-        //playerTurnDiv.appendChild()
 
-// RESET BUTTON
-// const clear = document.getElementById("clear");
-// clear.addEventListener("click", () => {
-//     for (let j=0; j < gameTiles.length; j++) {
-//         let tile = gameTiles[j].innerText;
-//         gameTiles[j]
-//     }
-// })
-    //// capture reset button
-    /// if reset button is clicked reset game board
-    //document.querySelector("#clear").addEventListener('click', () => {
-    // gameTiles.value = " "
-    //   })
+
+    const clear = document.getElementById("clear");
+    clear.addEventListener("click", (event) => {
+        for (let j=0; j < gameTiles.length; j++) {
+        let tile = gameTiles[j].innerText;
+        gameTiles[j].pull(event.target.id)
+        event.target.innerText = " "
+    }
+})
 
     // DISPLAY MESSAGE
     //// add a blank div to gameboard
