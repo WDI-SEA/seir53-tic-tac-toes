144d37405477c00724c8d7f4ff8d5633d895502f
BeckyBoo88 <rbooco07@gmail.com>
2021-05-10 15:49:27 -0600

    STTT

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
    let playerTurns = 0
+  // BELOW FOLLOWING ANNA'S EXAMPLE; ABOVE WORKS FOR X AND O TURNS
+   let playerX = ("X", [])
+   let playerO = ("O", [])
 
  // ARRAY FOR SQUARES
     let s1 = document.getElementById('one')
@@ -12,35 +15,32 @@ window.addEventListener('DOMContentLoaded', () => {
     let s8= document.getElementById('eight')
     let s9= document.getElementById('nine')
 
-    const gameSq= [s1,s2,s3,s4,s5,s6,s7,s8]
+    const gameSq= [s1,s2,s3,s4,s5,s6,s7,s8,s9]
     const gameSquareArray= document.querySelectorAll('.gameTile')
-    console.log(`gameSq ${gameSq}`)
-    console.log(`gameSq ${gameSquareArray}`)
+    // console.log(`gameSq ${gameSq}`)
+    // console.log(`gameSq ${gameSquareArray}`)
 
-// WIN.LOSE.TIE CONDITIONAL
+//WIN.LOSE.TIE CONDITIONAL
 
-    // const win= {
-    //     scenario1 : (s1,s2,s3),
-    //     scenario2 : (s4,s5,s6),
-    //     scenario3 : (s7,s8,s9),
-    //     scenario4 : (s1,s4,s7),
-    //     scenario5 : (s2,s5,s3),
-    //     scenario6 : (s3,s6,s9),
-    //     scenario7 : (s1,s5,s9),
-    //     scenario8 : (s3,s5,s7)
-    // }
-
-    // if (win === true  "X") {
-    //     console.log("X wins! O is oooo so sad");
-    // } else if (win === true) : 'O' {
-    //     console.log("O wins! X is xxxceptionally distraught");
-    // } else {
-    //     console.log("Tie! Everyone's a winner!! kinda..")
-    // }
-   
-   
-   
-   
+    const winCombo = [
+         [s1, s2, s3],
+         [s4, s5, s6],
+         [s7, s8, s9],
+         [s1, s4, s7],
+         [s2, s5, s3],
+         [s3, s6, s9],
+         [s1, s5, s9],
+         [s3, s5, s7]
+    ]
+    if (playerTurns >= 4) {
+        if (winCombo === playerX) {
+            console.log("X wins!");
+         } else if (winCombo === playerO) {
+        console.log("O wins!");
+        } else (playerTurns > 9) 
+        console.log("Tie! Everyone's a winner!! kinda..")
+         
+}
    
    
     // On click toggle between Xs and Os on the gameboard
@@ -49,20 +49,16 @@ window.addEventListener('DOMContentLoaded', () => {
    // Click event for tiles. Every tile is X unless already clicked, then 0
    for (let i=0; i < gameTiles.length; i++) {
        gameTiles[i].addEventListener('click', (event) => {
-           playerTurns++
            // playerTurns= playerTurns + 1
+            playerTurns++
+          // HOW TO CHECK IF CLASSLIST CONTAINS NO CLICK 
             event.target.classList.add('noClick')
           
-           // HOW TO CHECK IF CLASSLIST CONTAINS NO CLICK (NEXT STEP)
-            // if no click then "O"
-           //  let noMas = document.getElementsByClassName('noClick')
-             //noMas === "O" ? "O" : "X"  
-             
            // see what current letter is of game tile that was clicked
-           //let tile = gameTiles[i].innerText
+           let tile = gameTiles[i].innerText
           
            // toggle between whichever letter is current vs x or 0
-           let newPlayStatus = playerTurns % 2 === 0 ? "X" : "0"
+           let newPlayStatus = playerTurns % 2 === 0 ? "X" : "O"
            
            // set innerText of div to be new toggled letter
            gameTiles[i].innerText = newPlayStatus
