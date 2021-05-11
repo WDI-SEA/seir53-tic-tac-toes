127fea6d3e0fdd5f041dadcebab5511176f7b85d
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-05-11 00:14:46 -0400

    Uh i completely re-worked everything here and now it doesnt work

--- scripts/app.js
+++ scripts/app.js
@@ -1,92 +1,114 @@
 window.addEventListener("DOMContentLoaded", () => {
-    let playerXMoves = [];
-    let playerOMoves = [];
-    let totalMoves = 0;
+    let winnerBanner = document.getElementById("winnerCircle")
+    const userMessage = document.getElementById("userMessage")
     let gameTiles = document.querySelectorAll(".gameTile")
-    const clearButton = document.getElementById("clear")
+    userMessage.innerText = "Player X goes first."
 
-    for(let i = 0; i < gameTiles.length; i++) {
-        gameTiles[i].addEventListener("click", (event) => {
-        const userMessage = document.getElementById("userMessage")
-        if (!event.target.classList.contains("clicked")) {
-            event.target.className += " clicked";
-            totalMoves++;
-            event.target.innerText = totalMoves % 2 === 0 ? "X" : "O"
-            if (totalMoves % 2 === 0) {
-                event.target.innerText = "O";
-                playerXMoves.push(event.target.id)
-                console.log(playerXMoves)
-                userMessage.innerText = "Player X Turn";
-            } else {
-                event.target.innerText = "X";
-                playerOMoves.push(event.target.id)
-                console.log(playerOMoves)
-                userMessage.innerText = "Player O Turn"
-            }
-        }
-    })
-    }//function boxClick(event) {
-    //let tileCheck = event.target
-   // if (tileCheck.classList.contains("playedX") || tileCheck.classList.contains(playedO)) {
-   // displayTurn.innerText = "Tile already played. Click again"
-   //} else  { 
-    // if (turnNu  % 2 === 0) {
-    //      tileCheck.classList.add("played X")         <<<<<<
-    //      tileCheck.innerText = "X"
-    //      userMessage.innerText = "player 0 turn"
-    //      playerX.push(tileCheck.id)                   <<<<
-   // } else {
-   // }
-   // if (totalMoves >= 4) {
-    //  gameOver()
-   //}
-   //if (totalMoves === 9) {
-    // winnerCircle.innerText = "GAME TIED"
-    // userMessage.innerText = "Play Again"
-   // endGame()
-   //}
-   //
-    const winningArray =[       //     function gameOver() {
-        ["one", "two", "three"],    //
-        ["four", "five", "six"],     //}
-        ["seven", "eight", "nine"],  //
+    let playerXMoves = []
+    let playerOMoves = []
+    let totalMoves = 0
+
+    const winningArray = [
+        ["one", "two", "three"],    
+        ["four", "five", "six"],    
+        ["seven", "eight", "nine"],  
         ["one", "four", "seven"],
         ["two", "five", "eight"],
         ["three", "six", "nine"],
         ["three", "five", "seven"],
         ["one", "five", "nine"],
     ]
-    
+    gameTiles.addEventListener("click", boxClick)
+    function boxClick(event) {
+        let tileCheck = event.target
+        if (tileCheck.classList.contains("playedX") || tileCheck.classList.contains("playedO")) {
+            userMessage.innerText = "Tile already played. Click again."
+        } else {
+            if (totalMoves % 2 === 0) {
+                tileCheck.innerText = "X"
+                tileCheck.classList.add("playedX")
+                userMessage.innerText = "Player O Turn"
+                totalMoves++
+                playerXMoves.push(tileCheck.id)
+            } else {
+                tileCheck.innerText = "O"
+                playerOMoves.push(tileCheck.id)
+                tileCheck.classList.add("played O") 
+                userMessage.innerText = "Player X Turn"
+                totalMoves++
+            }
+         if (totalMoves >= 4) {
+            endGame()
+         }
+         if (totalMoves === 9) {
+        winnerBanner.innerText = "GAME TIED"
+        userMessage.innerText = "Play again?"
+        endGame()
+        stopGame()
+        }
+    }
+}
 
-    const playerArray = Array.from(document.getElementsByClassName("gametile"));
 
-    console.log(playerArray.includes(["one", "two", "three"]));
-    //let winnerBanner = document.getElementById("winnerCircle");
-   // let checkIfPlayerWins = (playerArray, target) => target.every(t ); =>  
-
-   //     function checkIfPlayerWins (playerArray) {
+function endGame () {
+    for (let i = 0; i < winningArray.length; i++) {
+      let winFoundX = 0
+      let winFoundO = 0
+      for (let y = 0; y < winningArray[i].length; y++) {
+        if (playerXMoves.includes(winningArray[i][y])) {
+          winFoundX++
+          if (winFoundX === 3) {
+            userMessage.innerText = "  "
+            winnerCircle.innerText = "Player X won!"
+            stopGame()
+          }
+        }
+         if (playerOMoves.includes(winningArray[i][y])) {
+        winFoundO++
+        if (winFoundO === 3) {
+          winnerCircle.innerText = "Player o has won!!"
+          userMessage.innerText = "   "
+          stopGame()
+        }
+      }
+    }
+  }
+}
+  function stopGame() {
+    gameTiles.removeEventListener('click', boxClick)
+  }
+})
+//tileCheck.innerText = totalMoves % 2 === 0 ? "X" : "O"
+// if (tileCheck.classList.contains("playedX") || tileCheck.classList.contains(playedO)) {
+// userMessage.innerText = "Tile already played. Click again"
+//} else  { 
+// if (turnNu  % 2 === 0) {
+//      tileCheck.classList.add("played O")         <<<<<<
+//      tileCheck.innerText = "X"
+//      userMessage.innerText = "player 0 turn"
+//      playerX.push(tileCheck.id)                   <<<<
+// } else {
+//if (totalMoves === 9) {
+//stopGame()
+// if (userMessage.innerText !== "") {}
+//winnerCircle.innerText = "GAME TIED"
+// userMessage.innerText = "Play Again"
+// stopGame()
+// let checkIfPlayerWins = (playerArray, target) => target.every(t ); =>  
+//     function checkIfPlayerWins (playerArray) {
 //         let (i = 0; i < playerArray.length; i++) { 
 //     if (playerXMoves === playerArray[i] || playerOMoves === playerArray[i]) {
 //         console.log("this player won!!");
 //     }    let winnerBanner = document.getElementById("winnerCircle");
-// }}
-//}
-// //  }    
-//gameTiles.forEach(() => {
-//addEventListener("click", (event) => {
-//gameTiles.innerText[i]
-// let currentStatus = event.target.innerText;
-// let newPlayStatus = currentStatus === "X" ? "0" : "X"
-// event.target.innerHTML = newPlayStatus//detect Draw("Tied game""No winner") conditions 
-//once 9 clicks detected,  run Tie function
-   
- //   clearButton.addEventListener("click", function resetGameboard() {
- //       event.preventDefault()
-  //      event.target.innerText = " "
-//         let tile = gameTiles[j].innerText;
-//         gameTiles[j].pull(event.target.id)
-// }
-     })
-
-
-    
+//    const clearButton = document.getElementById("clear")
+// clearButton.addEventListener("click", function resetGameboard() {
+//     gameTiles = document.querySelectorAll(".gameTile")
+//     for (let i = 0; i < gameTiles.length; i++) {
+//         gameTiles[i].classList = "gameTile"
+//         gameTiles[i].innerText = "  "
+//     }
+//     userMessage.innerText = "Player x goes first"
+//     winnerBanner.innerText = "  "
+//     playerXMoves = [];
+//     playerOMoves = [];
+//     totalMoves = 0;
